0c1abce2f2332820ff6a155fd0c0c99419d715c3
24wangchen <michael.chen198854@gmail.com>
2019-12-24 00:15:09 +0800

    init

---
+++ simple/Roman_to_Integer/Solution.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public static void main(String[] args) {
+    String input = "LVIII";
+    Map<Character, Integer> map = new HashMap();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+    int len = input.length();
+    int result = map.get(input.charAt(len - 1));
+    for (int i = len - 2;  i >= 0; i--) {
+      if (map.get(input.charAt(i)) >= map.get(input.charAt(i + 1))) {
+        result += map.get(input.charAt(i));
+      } else {
+        result -= map.get(input.charAt(i));
+      }
+    }
+    System.out.println(result);
+  }
+}
